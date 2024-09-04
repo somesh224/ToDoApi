06633648d20362eec48a7ae69f90a8d897e01c68
somesh224 <someshmishra224@gmail.com>
2024-09-04 15:24:33 +0530

    Added put, delete operation for tasks

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const app = express();
 
-dotenv.config({
-    path: './config/config.env'
-});
-
 app.use(morgan('dev'));
 
 app.use(express.json({}));
@@ -25,6 +21,7 @@ connectDB();
 //GET, POST, DELETE, PUT
 
 app.use('/api/todo/auth', require('./routes/user'));
+app.use('/api/todo', require('./routes/todo'));
 
 const PORT = process.env.PORT || 3000;
 
